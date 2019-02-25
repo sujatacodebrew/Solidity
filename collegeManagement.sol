ae9972fa5e2908e15deed5b9efb34463b39b2f68
Ubuntu <sujatacodebrew@gmail.com>
2019-02-25 17:29:51 +0530

    25th feb

--- collegeManagement.sol
+++ collegeManagement.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.1;
-pragma experimental ABIEncoderV2;
+//pragma experimental ABIEncoderV2;
 contract SchoolManagement{
     address public owner;
     constructor() public{
@@ -7,37 +7,33 @@ contract SchoolManagement{
     }
     struct Student{
         string name;
-        uint rollNo;
         string class;
         uint age;
     }
-    string[] Name;
-    uint[] RollNo;
-    string[] Class;
-    uint[] Age;
+    //string[] Name;
+    // uint[] RollNo;
+    // string[] Class;
+    // uint[] Age;
+    uint public count=1;
     modifier onlyOwner(){
         require(owner==msg.sender);
         _;
     }
-    Student[] allStudent;
-    function addStudent(string memory _sname, uint _srollNo, string memory _sclass, uint _sage) public onlyOwner{
-        Student memory newstudent = Student({name : _sname,
-        rollNo : _srollNo,
+    mapping(uint => Student) allStudent;
+    
+    function addStudent(uint rollNo, string memory name, string memory _sclass, uint _sage) public onlyOwner{
+        Student memory newstudent = Student({name : name,
         class : _sclass,
         age : _sage});
-        allStudent.push(newstudent);
+        allStudent[rollNo]=newstudent;
+        count++;
     }
     // function removeStudent(uint index) public onlyOwner{
     //     delete allStudent[index];
     // }
-    function viewStudent(uint index) external view returns(Student memory){
-        require(index<allStudent.length);
-        return(allStudent[index]);
+    function viewStudent(uint rollNo) external view returns (string memory,string memory,uint) {
+        require(count>0);
+        return (allStudent[rollNo].name,allStudent[rollNo].class,allStudent[rollNo].age);
     }
-    //  function viewAllStudent() public view returns (Name,uint[] memory,string[] memory,uint[] memory){
-    //      uint i;
-    //      for(i=0; i<allStudent.length; i++){
-    //         name.push(allStudent[i].name);
-    //      }
-    //  }
-}
+    
+    
